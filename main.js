08948ea990bbf5eb732693aff62e57e7e3790490
viktorTrz <viktor_trz@abv.bg>
2013-11-16 16:08:17 +0200

    nov main

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
 var unitStats=[];
-unitStats["worker"] = new Stat(10, 1, 5);
 
-function Stat(hp, damage, speed){
+unitStats["worker"] = new Stat(10, 1, 5, 0);
+
+
+function Stat(hp, damage, speed, cappacity, image, ){
+
 	this.hp = hp;
 	this.damage = damage;
 	this.speed = speed;
+	this.cappacity = cappacity;
+	this.image = new Image();
+	this.image = image;
+	
 }
 
 function Vector(x,y){
 	this.x=x;
 	this.y=y;
 };
+
 var Map = {
 	size:new Vector ( 100 , 100 ) ;
 	value:create2dArray( size.x, size.y );
 };
+
 function  Unit (type) {
 	
 	this.type=type;
