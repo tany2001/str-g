3cefa0a6da04a385687eade235134e3980a8707e
Stansislav <tany2001@gmail.com>
2013-11-16 15:44:29 +0200

    nachalo

---
+++ game.js
@@ -0,0 +1,29 @@
+var requestAnimationFrame = window.requestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function (callback) { setTimeout (callback, 1000 / 30); };
+
+var canvas = document.getElementById("canvas-id");
+canvas.width = 800;
+canvas.height = 600;
+var context = canvas.getContext("2d");
+
+window.addEventListener("mousedown", function (args)
+{
+    var myX = args.clientX - canvas.offsetLeft;
+    var myY = args.clientY - canvas.offsetTop;
+}, false);
+
+function update()
+{
+    setTimeout(update, 10);
+}
+
+function draw()
+{    
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    requestAnimationFrame(draw);
+}
+update();
+draw();
