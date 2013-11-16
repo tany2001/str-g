3d45b8f5292aaf49c7f9569efe0e0174e5a7c1cc
Stansislav <tany2001@gmail.com>
2013-11-16 21:59:15 +0200

    Nainakraq smislen teren generator

--- trr.js
+++ trr.js
@@ -1,95 +1,45 @@
-/*function ch(a)
+function chance(a)
 {
 	var n = Math.random() * 100;
-	if(n < a)
+	if(n <= a)
 	{
 		return true;
 	}
 	return false;
 }
 
-//0 - water, 1 - dirt
-function generateTerrain()
+
+function generateMap()
 {
-	var any = true, chance = 100;
-	var next = []; next.push(new Vector(50, 50));
+	var next = [], ch = 100;
 	var used = create2dArray(map.size.x, map.size.y, false);
 
-	var moveX = [1, -1, 0, 0];
-	var moveY = [0, 0, 1, -1];
+	next.push(new Vector(map.size.x / 2, map.size.y / 2));
+	used[map.size.x / 2][map.size.y / 2] = true;
 
-	while(any)
+	var moveX = [0, 0, -1, 1];
+	var moveY = [1, -1, 0, 0];
+
+	while(next.length > 0)
 	{
-		var c = next[0]; next.remove(0);
+		var c = next[0];
+		next.remove(0);
+		map.value[c.x][c.y] = 1;
 
-		map.value.value[c.x][c.y] = 1; used[c.x][c.y] = true;
-		any = false;
-
-		for(var i = 0;i < moveX.length;i ++)
+		for(var a = 0;a < moveX.length;a ++)
 		{
-			if(c.x + moveX[i] >= 0 && c.x + moveX[i] < map.size.x && c.y + moveY[i] >= 0 && c.y + moveY[i] < map.size.y && !used[c.x + moveX[i]][c.y + moveY[i]])
+			var n = new Vector(c.x + moveX[a], c.y + moveY[a]);
+			if(n.x >= 0 && n.y >= 0 && n.x < map.size.x && n.y < map.size.y && !used[n.x][n.y])
 			{
-				if(Math.floor(Math.random() * 1000) > 3)
+				if(chance(ch))
 				{
-					any = true;
-					next.push(new Vector(c.x + moveX[i], c.y + moveY[i]));
-					used[c.x + moveX[i]][c.y + moveY[i]] = true;
-				}
-			}
-		}
-
-		chance -= 0.001;
-	}
-}
-
-generateTerrain();
-
-*/
-
-function generateMap(sx, sy, maxSize)
-{
-	map.value[sx][sy] = 0;
-	var createdBlocks = 0;
-	for(var br = 1;br < maxSize;br ++)
-	{
-		for(var x = 0;x < map.size.x;x ++)
-		{
-			for(var y = 0;y < map.size.y;y ++)
-			{
-				var choose = Math.floor(Math.random() * 2.8);
-				if(x - 1 >= 0 && map.value[x][y] == br - 1 && choose == 0)
-				{
-					map.value[x - 1][y] = br;
-					createdBlocks ++;
-				}
-				if(y - 1 >= 0 && map.value[x][y] == br - 1 && choose == 0)
-				{
-					map.value[x][y - 1] = br;
-					createdBlocks ++;
-				}
-				if(x + 1 < map.size.x && map.value[x][y] == br - 1 && choose == 0)
-				{
-					map.value[x + 1][y] = br;
-					createdBlocks ++;
-				}
-				if(y + 1 < map.size.x && map.value[x][y] == br - 1 && choose == 0)
-				{
-					map.value[x][y + 1] = br;
-					createdBlocks ++;
+					used[n.x][n.y] = true;
+					next.push(new Vector(n.x, n.y));
+					ch -= 0.01;
 				}
 			}
 		}
 	}
-	return createdBlocks;
 }
 
-generateMap(map.size.x / 2, map.size.y / 2, 1.00000000001);
-
-/*var again = false;
-while(again == false)
-{
-	if(generateMap(map.size.x / 2, map.size.y / 2, 60) > 200)
-	{
-		again = true;	
-	}
-}*/
+generateMap();
