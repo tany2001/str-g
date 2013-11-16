646e562aa983e538bb63cfbd9e3a11da2ef2f3c3
viktorTrz <viktor_trz@abv.bg>
2013-11-16 16:32:19 +0200

    po-nov main

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
-var unitStats=[];
+var unitStats=[],building;
 
-unitStats["worker"] = new Stat(10, 1, 5, 0);
+function create2dArray(sizeX,sizeY,value){
+	var x,y,array=[];
+	for (y=0;y<sizeY;y++){
+		array[y]=[];
+		for (x=0;x<sizeX;x++){
+			array[y][x] = value;
+		}
+	}
+}
 
+unitStats["worker"] = new Stat(10, 1, 5, 0 ,/*img*/ , 1 , 1);
 
-function Stat(hp, damage, speed, cappacity, image, ){
+unitStats["soldier"] = new Stat(20, 2, 5 ,0 , /*img*/, 1 , 1)
+
+unitStats["small_ship"] = new Stat(50, 2, 8 , 10,/*img*/ , 1 , 1);
+
+unitStats["big_ship"] = new Stat(50, 2, 5 , 20 ,/*img*/ , 1 , 1);
+
+unitStats["main_building"] = new Stat(100 , 0 , 0 , 5 , /*img*/ , 2 , 2);
+
+function Stat(hp, damage, speed, cappacity, image , sizeX, sizeY){
 
 	this.hp = hp;
 	this.damage = damage;
@@ -11,7 +28,7 @@ function Stat(hp, damage, speed, cappacity, image, ){
 	this.cappacity = cappacity;
 	this.image = new Image();
 	this.image = image;
-	
+	this.size = new Vector(sizeX, sizeY);
 }
 
 function Vector(x,y){
@@ -21,14 +38,12 @@ function Vector(x,y){
 
 var Map = {
 	size:new Vector ( 100 , 100 ) ;
-	value:create2dArray( size.x, size.y );
+	value:create2dArray( size.x, size.y, 0);
+	unit:create2dArray( size.x, size.y, 0);
 };
 
 function  Unit (type) {
 	
-	this.type=type;
-	this.hp=unitStats[type].hp;
-	this.damage=unitStats[type].damage;
-	this.speed=unitStats[type].speed;
+	this = unitStats[type];
 	
 };
